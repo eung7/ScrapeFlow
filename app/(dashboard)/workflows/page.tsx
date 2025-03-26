25483661366a40222367fb2eb98d00b000ceadac
Eunzo <wannabeeung@gmail.com>
2025-03-26 21:33:00 +0900

    💄 Add Description Text When Workflow is empty

--- app/(dashboard)/workflows/page.tsx
+++ app/(dashboard)/workflows/page.tsx
@@ -1,5 +1,8 @@
+import { getWorkflowsForUser } from "@/actions/workflows/getWorkflowsForUser";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
 import { waitFor } from "@/lib/helper/waitFor";
+import { AlertCircle, InboxIcon } from "lucide-react";
 import React, { Suspense } from "react";
 
 function page() {
@@ -32,7 +35,36 @@ function UserWorkFlowsSkeleton() {
 }
 
 async function UserWorkflows() {
-  await waitFor(3000);
+  const workflows = await getWorkflowsForUser();
+
+  if (!workflows) {
+    return (
+      <Alert variant={"destructive"}>
+        <AlertCircle className="w-4 h-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription>
+          Something went wrong. Please try again later
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (workflows.length === 0) {
+    return (
+      <div className="flex flex-col gap-4 h-full items-center justify-center">
+        <div className="rounded-full bg-accent w-20 h-20 flex items-center justify-center">
+          <InboxIcon size={40} className="stroke-primary" />
+        </div>
+        <div className="flex flex-col gap-1 text-center">
+          <p className="font-bold">No workflow created yet</p>
+          <p className="text-sm text-muted-foreground">
+            Click the button below to create your first workflow
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return <div></div>;
 }
 
