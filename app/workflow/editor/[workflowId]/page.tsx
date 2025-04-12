5cbe709cffe3e0f496642d148a1f8fbc0c2e4974
Eunzo <wannabeeung@gmail.com>
2025-04-12 17:56:24 +0900

    ⚡️ Add workflow editor

---
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -0,0 +1,25 @@
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+import Editor from "../../_components/Editor";
+
+async function page({ params }: { params: { workflowId: string } }) {
+  const { workflowId } = params;
+  const { userId } = await auth();
+
+  if (!userId) {
+    return <div>Unauthorized</div>;
+  }
+
+  const workflow = await prisma.workflow.findUnique({
+    where: { id: workflowId, userId: userId },
+  });
+
+  if (!workflow) {
+    return <div>Workflow not found</div>;
+  }
+
+  return <Editor workflow={workflow} />;
+}
+
+export default page;
