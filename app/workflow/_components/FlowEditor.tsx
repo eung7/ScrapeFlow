cd977b1baa002b09e7c20ab4e45cda82dd13c43c
Eunzo <wannabeeung@gmail.com>
2025-04-14 22:48:29 +0900

    ✨  Extract text from element task

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -11,9 +11,18 @@ import {
 } from "@xyflow/react";
 import React from "react";
 import "@xyflow/react/dist/style.css";
+import { createFlowNode } from "@/lib/workflow/createFlowNode";
+import { TaskType } from "@/types/task";
+import NodeComponent from "./nodes/NodeComponent";
+
+const nodeTypes = {
+  FlowScrapeNode: NodeComponent,
+};
 
 function FlowEditor({ workflow }: { workflow: Workflow }) {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([
+    createFlowNode(TaskType.LAUNCH_BROWSER),
+  ]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   return (
@@ -23,6 +32,7 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
+        nodeTypes={nodeTypes}
       >
         <Controls position="top-left" />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
